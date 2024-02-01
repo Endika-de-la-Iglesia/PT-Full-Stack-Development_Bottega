eb305b07711ef3ff2347a9ceefde8d492fa95d3d
Endika-de-la-Iglesia <endika.delaiglesia@gmail.com>
2024-02-01 14:53:27 +0100

    Uploading exercise M2C3 Python Assignment

---
+++ M2C3 Python Assignment/exercise.py
@@ -0,0 +1,46 @@
+# Exercise 1
+string_one = "Hola, ¿qué tal?"
+num_one = 12
+list_one = ["Hola", 12, "adiós", 20]
+boolean_one = True
+
+# Exercise 2
+three_letters = string_one[0:3]
+
+# Exercise 3
+first_el_list_one = list_one[0]
+
+# Execise 4
+new_num = num_one + 10
+
+# Exercise 5
+last_el_list_one = list_one[-1]
+
+# Exercise 6
+names = 'harry,alex,susie,jared,gail,conner'
+name_list = names.split(",")
+
+# Exercise 7
+first_word = string_one[0:4].upper()
+new_string = first_word + string_one[4:]
+
+# Exercise 8
+print(f"Ahora tengo {num_one} años, dentro de 10, tendré {new_num}.")
+
+
+# Exercise 9 
+print("hello world")
+
+# All the other results printed to check that they work
+print("\n\n")
+print(string_one)
+print(num_one)
+print(list_one)
+print(boolean_one)
+print(three_letters)
+print(first_el_list_one)
+print(new_num)
+print(last_el_list_one)
+print(name_list)
+print(first_word)
+print(new_string)
